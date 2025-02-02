67eddd45a3a38926c3bad17997f66414954dea50
Jason L Causey <11724284+jcausey-astate@users.noreply.github.com>
2025-02-02 12:21:34 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+# borg-archive.sh
+
+A wrapper that allows [Borg Backup](https://www.borgbackup.org/) to be used to create single-file compressed archives.
+
+Basic functions are supported creating, mount/extract, and update of the archive.
+
+The primary use-case is sharing and archiving versioned datasets for research and similar purposes.
+
+I've been using the `.baz` extension for these archives, but you can do whatever you like. 😄
+
+## Installation
+
+### Requirements
+
+You will need a working installation of [Borg Backup](https://www.borgbackup.org/).  This script is just a convenience wrapper around Borg and `tar`.
+
+If you want to be able to use the `mount` functionality, you will also need a working installation of FUSE (See https://en.wikipedia.org/wiki/Filesystem_in_Userspace).  If you are on a Mac, you might want [macFuse](https://macfuse.github.io/).
+
+### Installing
+
+The script `borg-archive.sh` contains all functional code.  It is a Bash script, so you will need to run it under a bash shell.  To install it for all users, you could copy it to e.g. `/usr/local/sbin/` and maybe symbolic link the shorter name `borg-archive`.  To install for your own user account, you might copy it to `~/.local/sbin/` or similar.
+
+## Usage
+
+* `borg_archive.sh create archive_file.baz  /path/to/archive-root-dir [encryption] [borg-options]`
+  * Initializes and creates initial archive.
+  * `encryption`: Turns on encryption of the archive.
+  * `borg-options`: Fine-tune underlying Borg Backup parameters.
+
+* `borg_archive.sh extract archive_file.baz  /path/to/destination [tag]`
+  * Extracts the archive to 'destination'. 'destination' will be created
+    if it does not exist. If it exists, current contents will be
+    _OVERWRITTEN_!
+  * `tag`: Name of a commit tag to extract (default is most recent).
+
+* `borg_archive.sh help`  (or `--help` or `-h`)
+    * Prints usage information.
+
+* `borg_archive.sh list archive_file.baz`
+        Lists all available commits in this archive.
+
+* `borg_archive.sh mount archive_file.baz  /path/to/mount-dir [tag]`
+  * Mounts read-only to 'mount-dir'. mount-dir will be created if it
+        does not exist.
+  * `tag`: Name of a commit tag to extract (default is most recent).
+
+* `borg_archive.sh umount /path/to/mount-dir`
+  * Unmount an archive that was mounted with 'mount'.
+
+* `borg_archive.sh update archive_file.baz  /path/to/archive-root-dir [tag]`
+  * Update the archive with any changes in 'archive-root-dir'.
+  * tag: Used to name the commit, if omitted the tag is generated
+    automatically as an incrementing counter.
+
+## Inspiration
+
+This script was inspired as a continuation of my search for new and better? ways to store and share versioned datasets.  I thought of the question:  What if Borg could create a tarball-like file instead of a directory?  After a little searching, I found the following thread online:
+
+* [https://github.com/borgbackup/borg/issues/4808](https://github.com/borgbackup/borg/issues/4808)
+
+Where jpfleury was asking basically the same question.  The Borg maintainers understandably saw this option as too complex and outside their scope, ending with "If you need that composition, just write a shell script.".  Well, I couldn't find any evidence that Jean-Philippe had done that, so I decided to take a shot at it.  This repo is the proof-of-concept for that.
+
+## What's next?
+
+Probably nothing... But this experience has gotten me thinking that this tool could be expanded to allow a very Git-like workflow for datasets.  My hangup is the prerequisite software users would have to install.  To do this right, it needs to be self-contained.  And I don't have time to do that right now, so it'll have to wait.
---
+++ borg-archive.sh
@@ -0,0 +1,268 @@
+#! /usr/bin/env bash
+# borg-archive.sh
+#   A wrapper that allows Borg Backup to be used to create single-file
+#   compressed archives.  Basic functions are supported creating, mount/extract,
+#   and update of the archive.  Primary use-case is sharing and archiving
+#   versioned datasets for research and similar purposes.
+#
+# MIT License
+#
+# Copyright 2025 Jason L. Causey 
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the “Software”), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions: 
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.  
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ------------------------------------------------------------------------------
+
+borg_archive=$(basename "$0")
+function usage() {
+    cat <<EOF
+
+Usage:
+    $borg_archive create archive_file.baz  /path/to/archive-root-dir [encryption] [borg-options]
+        Initializes and creates initial archive.
+        encryption: Turns on encryption of the archive.
+        borg-options: Fine-tune underlying Borg Backup parameters.
+
+    $borg_archive extract archive_file.baz  /path/to/destination [tag]
+        Extracts the archive to 'destination'. 'destination' will be created 
+            if it does not exist. If it exists, current contents will 
+            be OVERWRITTEN!
+        tag: Name of a commit tag to extract (default is most recent).
+
+    $borg_archive list archive_file.baz
+        Lists all available commits in this archive.
+
+    $borg_archive mount archive_file.baz  /path/to/mount-dir [tag]
+        Mounts read-only to 'mount-dir'. mount-dir will be created if it 
+            does not exist.
+        tag: Name of a commit tag to extract (default is most recent).
+
+    $borg_archive umount /path/to/mount-dir
+        Unmount an archive that was mounted with 'mount'.
+
+    $borg_archive update archive_file.baz  /path/to/archive-root-dir [tag]
+        Update the archive with any changes in 'archive-root-dir'.
+        tag: Used to name the commit, if omitted the tag is generated
+            automatically as an incrementing counter.
+
+EOF
+}
+
+function extract_tarfile {
+    [[ -z $1 ]] && {
+        echo "TARFILE name not provided."
+        exit 98
+    }
+    [[ -z $2 ]] && {
+        echo "WORKDIR name not provided."
+        exit 98
+    }
+    tar -xf "${1}" -C "${2}" --strip-components=1
+}
+
+trap '{ [[ "$ACTION" != "mount" ]] && rm -rf -- "$WORKDIR" 2> /dev/null; }' EXIT
+set -e
+WORKDIR="$(mktemp -d)"
+ACTION="$1"
+
+case $ACTION in
+create | extract | list | mount | update)
+    shift
+    ;; # OK
+umount | unmount)
+    ACTION='umount'
+    shift
+    ;; # OK, and standardize 'umount'
+--help | -h | help)
+    usage
+    exit 0
+    ;; # OK, show help then exit
+*)
+    echo "First argument must be create, extract, help, list, mount, or update.  Saw '${ACTION}'."
+    usage
+    exit 1
+    ;;
+esac
+
+if [ -z "$1" ]; then
+    if [[ $ACTION != "umount" ]]; then
+        echo "Archive file name is required."
+        usage
+        exit 1
+    fi
+fi
+
+ARCHFILE="${1}"
+shift
+
+# Perform the requested action
+case $ACTION in
+create) # CREATE NEW ARCHIVE
+    if [ -z "$1" ]; then
+        echo "Path to root of archive directory must be given."
+        exit 2
+    fi
+    SOURCEDIR="$1"
+    shift
+
+    # Set the encryption mode, which is the only REQUIRED option for Borg.
+    ENCR_MODE="none"
+    if [ "$1" == "encryption" ] || [ "$1" == "-e" ] || [ "$1" == "--encryption" ]; then
+        if [ "$1" == "encryption" ]; then
+            ENCR_MODE="repofile"
+            shift
+        else
+            ENCR_MODE="$2"
+            shift
+            shift
+        fi
+    fi
+
+    # We are initializing a tag list here. We always use "1" as the initial commit tag.
+    TAG=1
+    echo "${TAG}" >"${SOURCEDIR}/.ba-tags"
+
+    borg init --encryption "${ENCR_MODE}" "$@" "${WORKDIR}"
+    OLDDIR="$(pwd)"
+    cd "${SOURCEDIR}/.."
+    borg create "${WORKDIR}::${TAG}" "$(basename "${SOURCEDIR}")"
+    cd "${WORKDIR}/.."
+    tar -cvzf "${OLDDIR}/${ARCHFILE}" "$(basename "${WORKDIR}")"
+    cd "${OLDDIR}"
+    ;;
+
+extract) # EXTRACT most recent or specified [TAG] to specified directory
+    OUTPUTDIR="${1}"
+    shift
+    [[ -d "${OUTPUTDIR}" ]] && {
+        read -p "${OUTPUTDIR} already exists.  Contents will be overwritten."$'\n'"Are you sure? [y|N]  " -n 1 -r
+        if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+            echo "Stopping."
+            exit 0
+        fi
+        echo
+    }
+    mkdir -p "${OUTPUTDIR}" || {
+        echo "Cannot create output directory '${OUTPUTDIR}'."
+        exit 3
+    }
+    extract_tarfile "${ARCHFILE}" "${WORKDIR}"
+    OLDDIR="$(pwd)"
+    cd "${OUTPUTDIR}"
+
+    TAG="${1}"
+    if [ -z "${TAG}" ]; then
+        TAG=$(BORG_RELOCATED_REPO_ACCESS_IS_OK="yes" borg list "${WORKDIR}" --short --last 1 2>/dev/null) # default to latest
+    fi
+
+    BORG_RELOCATED_REPO_ACCESS_IS_OK="yes" borg extract --error "${WORKDIR}::${TAG}" 2>/dev/null
+    cd "${OLDDIR}"
+
+    echo "Extracted archive contents to '${OUTPUTDIR}'."
+    ;;
+
+list) # LIST the commit tags associated with this archive.
+    extract_tarfile "${ARCHFILE}" "${WORKDIR}"
+    BORG_RELOCATED_REPO_ACCESS_IS_OK="yes" borg list --format="{archive:<36} {time}{NL}" "${WORKDIR}" 2>/dev/null
+    ;;
+
+mount) # MOUNT most recent or specified [TAG] to specified directory (read-only)
+    if [ -z "$1" ]; then
+        echo "Must provide path to directory to mount."
+        usage
+        exit 4
+    fi
+    MOUNTDIR="${1}"
+    shift
+    [[ -d ${MOUNTDIR} ]] || mkdir "${MOUNTDIR}"
+    [[ -d ${MOUNTDIR} ]] || {
+        echo "Cannot create mount directory '${MOUNTDIR}'."
+        exit 4
+    }
+
+    # Save the path to the temp dir in the mount directory so we can clean up on unmount
+    echo "${WORKDIR}" >"${MOUNTDIR}/.borg-repo"
+    extract_tarfile "${ARCHFILE}" "${WORKDIR}"
+
+    TAG="${1}"
+    if [ -z "${TAG}" ]; then
+        TAG=$(BORG_RELOCATED_REPO_ACCESS_IS_OK="yes" borg list "${WORKDIR}" --short --last 1 2>/dev/null) # default to latest
+    fi
+    BORG_RELOCATED_REPO_ACCESS_IS_OK="yes" borg mount --error "${WORKDIR}::${TAG}" "${MOUNTDIR}" 2>/dev/null
+
+    echo "Mounted archive to '${MOUNTDIR}' (read-only)."
+    ;;
+
+umount)
+    # This is the only case where we don't have a real ARCHFILE.
+    # That variable holds the mount point here.
+    if [ -z "$ARCHFILE" ]; then
+        echo "Must provide path to mounted archive to unmount."
+        usage
+        exit 5
+    fi
+    # The steps here must happen in the right order to clean everything up.
+    rm -rf "${WORKDIR}"                     # delete current WORKDIR; we don't need it.
+    MOUNTDIR="${ARCHFILE}"                  # pull into correct variable for code clarity
+    borg umount "${MOUNTDIR}"               # unmount the borg repo
+    WORKDIR=$(cat "${MOUNTDIR}/.borg-repo") # .borg-repo becomes visible after unmount, get the temp dir name
+    rm "${MOUNTDIR}/.borg-repo"             # clean up the metadata file
+    echo "Unmounted archive."
+    ;;
+
+update)
+    if [ -z "$1" ]; then
+        echo "Path to root of archive directory must be given."
+        exit 6
+    fi
+    SOURCEDIR="$1"
+    shift
+    if [ -z "$1" ]; then
+        NTAGS=$(wc -l <"${SOURCEDIR}/.ba-tags")
+        TAG=$((NTAGS + 1))
+    else
+        TAG="$1"
+    fi
+    echo "${TAG}" >>"${SOURCEDIR}/.ba-tags"
+
+    # Extract existing archive to the WORKDIR:
+    extract_tarfile "${ARCHFILE}" "${WORKDIR}"
+    # The following steps match the "create" routine, except that we are careful
+    # not to overwrite the previous archive until creating the new one succeeds.
+    OLDDIR="$(pwd)"
+    cd "${SOURCEDIR}/.."
+    BORG_RELOCATED_REPO_ACCESS_IS_OK="yes" borg create "${WORKDIR}::${TAG}" "$(basename "${SOURCEDIR}")" 2>/dev/null
+    cd "${WORKDIR}/.."
+    # shellcheck disable=SC2015
+    tar -cvzf "${OLDDIR}/.${ARCHFILE}.working" "$(basename "${WORKDIR}")" &&
+        mv "${OLDDIR}/.${ARCHFILE}.working" "${OLDDIR}/${ARCHFILE}" ||
+        {
+            echo "Failed to update archive."
+            cd "${OLDDIR}"
+            exit 6
+        }
+    cd "${OLDDIR}"
+    echo "Archive updated successfully."
+    ;;
+
+*)
+    echo "Unknown action."
+    exit 99
+    ;;
+
+esac
